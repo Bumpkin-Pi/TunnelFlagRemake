ae31b1d06d8aa0d16d5bdb6fdf6790cd84ec192e
holly <h.person.notrobot@gmail.com>
2024-03-16 14:57:11 +0000

    Changed back to cmake files output.

--- src/loadTextures.cpp
+++ src/loadTextures.cpp
@@ -16,9 +16,9 @@
 
 Textures LoadTextures(SDL_Renderer *renderer) {
     Textures textures{};
-    textures.player1 = IMG_LoadTexture(renderer, "textures/player1.png"); //TODO: Add error correction and better expandability to this
-    textures.player2 = IMG_LoadTexture(renderer, "textures/player2.png");
-    textures.background = IMG_LoadTexture(renderer, "textures/background.png");
+    textures.player1 = IMG_LoadTexture(renderer, "../textures/player1.png"); //TODO: Add error correction and better expandability to this
+    textures.player2 = IMG_LoadTexture(renderer, "../textures/player2.png");
+    textures.background = IMG_LoadTexture(renderer, "../textures/background.png");
 
     return textures;
 }
